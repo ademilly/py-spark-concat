ba45e8bc9c6e79d556c07cbdaa633fd33f379a75
Aurélien Demilly <demilly.aurelien@gmail.com>
2016-11-17 13:52:25 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='py_spark_concat',
+    version='0.1',
+    description='Python concatener for spark output',
+    author='Aurélien Demilly',
+    author_email='demilly.aurelien@gmail.com',
+    url='https://github.com/ademilly/py-spark-concat',
+    license='MIT',
+    packages=['py_spark_concat'],
+)
