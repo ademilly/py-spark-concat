91aeec9df87acf3d175450a9d3670ee9cf0e5ddd
Aurélien Demilly <demilly.aurelien@gmail.com>
2016-11-17 14:00:32 +0100

    Fix pathing bug

--- py_spark_concat/concat.py
+++ py_spark_concat/concat.py
@@ -17,6 +17,6 @@ def concat_directory(path_to_dir, path_to_outfile):
             _ for _ in os.listdir(path_to_dir)
             if 'part' in _ and not _.endswith('.crc')
         ]:
-            with open(path_to_outfile + '/' + part_file):
+            with open(path_to_dir + '/' + part_file):
                 for line in part_file:
                     outfile.write(line)
